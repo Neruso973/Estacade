bf666b5ba39967316b81b4184c7073d01675b599
ClementDalrue <clement.dalrue@gmail.com>
2021-09-30 17:19:09 +0200

    anine page index

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,6 +7,47 @@ const dessert = document.querySelector(".dessert");
 let menuButton = document.querySelector('#menu-button');
 let menu = document.querySelector('#menu');
 
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
 // show or hide
 menuButton.addEventListener('click',function(){
 menu.classList.toggle('show-menu');
@@ -208,3 +249,4 @@ function goFish() {
 function goDessert() {
   document.querySelector(".dessert")?.scrollIntoView({behavior: 'smooth'}, );
 }
+

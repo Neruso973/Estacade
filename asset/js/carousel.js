a69f45cc21334a8d04d69c565be07cdda5ecd85e
boredwolf <felix.jouvhomme@gmail.com>
2021-10-01 16:43:04 +0200

    created carousel js file

---
+++ asset/js/carousel.js
@@ -0,0 +1,12 @@
+//jscript à propos carousel//
+
+const track = document.querySelector('.carousel-track');
+const nextButton = document.querySelector('.carousel-button-felix.arrow-right');
+const previousBUtton = document.querySelector('.carousel-button-felix.arrow-left');;
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from('dotsNav.children');
+// left click
+
+//right click
+
+//nav indicators click

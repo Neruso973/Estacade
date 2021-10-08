7cca24e0f265ea9e1c6686a54ccb24da5d1adf49
Neruso973 <nelson.ntepp@icloud.com>
2021-10-08 10:30:15 +0200

    fix img

--- asset/js/script.js
+++ asset/js/script.js
@@ -96,7 +96,7 @@ const chefMenu = [{
     {
         type: meat,
         name: 'Ris de Veau poêlés',
-        image: "asset/img/assiettes/Ris.png",
+        image: "asset/img/assiettes/ris.png",
         description: "sauce morilles, polenta crémeuse au mascarpone",
         price: "26€",
     },
